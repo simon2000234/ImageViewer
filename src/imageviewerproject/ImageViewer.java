23f8de88111d6c901162670b49de52546800d362
Simon Melchertsen <warcraft.simon@gmail.com>
2019-03-20 10:13:03 +0100

    Initial commit

---
+++ src/imageviewerproject/ImageViewer.java
@@ -0,0 +1,29 @@
+package imageviewerproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ImageViewer extends Application
+{
+    
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Image Viewer");
+        stage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    
+}
